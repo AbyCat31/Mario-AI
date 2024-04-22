6a5df702281b0165404954113174bc8940e3f987
AbyCat31 <130508413+AbyCat31@users.noreply.github.com>
2024-04-22 14:55:33 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
+img="";
+noseX=0;
+noseY=0;
+marioX=0;
+marioY=0;
 function preload() {
+	img=loadImage("mario05.png");
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
 }
 
 function setup() {
-	canvas = createCanvas(1240,336);
+	canvas = createCanvas(1800,500);
+	canvas.parent('canvas');
 	instializeInSetup(mario);
+	video=createCapture(VIDEO);
+	video.size(500, 400);
+	video.parent("game_console");
 }
 
 function draw() {
+	image(img, marioX, marioY, 40, 70);
 	game()
 }
 

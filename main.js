8e10a33584ee094eb3bcd35d9618549898e2cad3
AbyCat31 <130508413+AbyCat31@users.noreply.github.com>
2024-04-29 14:47:36 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -11,20 +11,33 @@ function preload() {
 }
 
 function setup() {
-	canvas = createCanvas(1800,500);
+	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
 	instializeInSetup(mario);
 	video=createCapture(VIDEO);
-	video.size(500, 400);
+	video.size(800, 400);
 	video.parent("game_console");
+	poseNet = ml5.poseNet(video, modelLoaded);
+	 poseNet.on('pose', gotPoses);
 }
 
 function draw() {
 	image(img, marioX, marioY, 40, 70);
 	game()
 }
-
-
+function modelLoaded()
+{
+	console.log("Tu modelo ha cargado");
+	
+}
+function gotPoses()
+{
+	if(results.length>0)
+	{
+		noseX=results[0].pose.nose.x;
+	noseY=results[0].pose.nose.y;
+	}
+}
 
 
 
